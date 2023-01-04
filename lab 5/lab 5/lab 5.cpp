fd1f8603c4139c029bf92b253f78580bf8d2611a
xpe1l <semenuk.boghdan@gmail.com>
2023-01-04 13:52:11 +0200

    lab 5

---
+++ lab 5/lab 5/lab 5.cpp
@@ -0,0 +1,15 @@
+﻿//вказівники і динамічний розподіл пам'яті
+
+#include <iostream>
+using namespace std;
+int main() {
+ 
+	float *ptrarray = new float[10];
+	for (int i = 0; i < 10; i++)
+		ptrarray[i] = rand() % 100 + 1;
+	for (int i = 0; i < 10; i++)
+		cout << ptrarray[i] << " ";
+	delete[] ptrarray;
+
+	return 0;
+}
